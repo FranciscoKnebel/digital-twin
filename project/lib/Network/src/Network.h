254e4d1027c5f5debc7cd5c65bcfa18c3fd6d563
Francisco Knebel <franciscopaivaknebel@gmail.com>
2019-09-09 15:27:49 -0300

    Device connect to wpa2-enterprise.

--- project/lib/Network/src/Network.h
+++ project/lib/Network/src/Network.h
@@ -1,12 +1,16 @@
 #ifdef ESP8266
     #include "ESP8266WiFi.h"
+    #include <user_interface.h>
 
     #define WIFI_AUTH_OPEN ENC_TYPE_NONE
 #endif
 
 #ifdef ESP32
     #include "WiFi.h"
+    #include "esp_wpa2.h"
 #endif
+
 
 void scanNetworks();
 void printNetwork(int i);
+
--- project/src/main.cpp
+++ project/src/main.cpp
@@ -1,14 +1,53 @@
 #include <Arduino.h>
 #include <Network.h>
 
+#define EAP_IDENTITY ""
+#define EAP_PASSWORD ""
+const char* ssid = "INF";
+
+const char* device;
+
+#if defined(DEVICE_ESP01)
+    device = ""
+#endif
+
 void setup()
 {
     Serial.begin(115200);
 
-    // Set WiFi to station mode and disconnect from an AP if it was previously connected
     WiFi.mode(WIFI_STA);
     WiFi.disconnect();
     delay(100);
+
+    int counter = 0;
+
+    #if defined(DEVICE_ESP01) || defined(DEVICE_ESP02)
+        esp_wifi_sta_wpa2_ent_set_identity((uint8_t *)EAP_IDENTITY, strlen(EAP_IDENTITY));
+        esp_wifi_sta_wpa2_ent_set_username((uint8_t *)EAP_IDENTITY, strlen(EAP_IDENTITY));
+        esp_wifi_sta_wpa2_ent_set_password((uint8_t *)EAP_PASSWORD, strlen(EAP_PASSWORD));
+        esp_wpa2_config_t config = WPA2_CONFIG_INIT_DEFAULT();
+        esp_wifi_sta_wpa2_ent_enable(&config);
+        WiFi.begin(ssid);
+    #endif
+
+    #if defined(DEVICE_ESP03)
+        struct station_config *config;
+        wifi_station_set_config(config);
+        wifi_station_set_wpa2_enterprise_auth(1);
+        wifi_station_set_enterprise_username((uint8_t *)EAP_IDENTITY, strlen(EAP_IDENTITY));
+        wifi_station_set_enterprise_password((uint8_t *)EAP_PASSWORD, strlen(EAP_PASSWORD));
+        wifi_station_connect();
+    #endif
+
+    while (WiFi.status() != WL_CONNECTED) {
+        delay(500);
+        Serial.print(".");
+        counter++;
+        
+        if(counter >= 60) {
+            ESP.restart();
+        }
+    }
 
     Serial.println("Setup done");
 }
